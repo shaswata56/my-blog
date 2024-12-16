a4f1de625679ca08b8b825d1cd9228ba4f85b448
shaswata56 <shaswata56@gmail.com>
2024-12-16 18:54:11 +0600

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   ssr: true,
   app: {
     baseURL: '/',
-    // buildAssetsDir: 'assets',
+    buildAssetsDir: 'assets',
   },
   routeRules: {
     '/': { prerender: true }
